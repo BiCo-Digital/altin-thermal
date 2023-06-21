b6ad6e53b050f3d211ff849e3d08f3e9c5bc3f9d
Matej <nevlud3@gmail.com>
2023-06-21 17:50:32 +0200

    working on linux

--- thermal_capture.py
+++ thermal_capture.py
@@ -1,45 +1,41 @@
+
 import cv2
+import numpy as np
 
-CAP_AVFOUNDATION = 1200
-CAP_DSHOW = 700
-CAP_ANY = 0
-# OpenCV video capture
-cap = cv2.VideoCapture(0)
 
-w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-fps = cap.get(cv2.CAP_PROP_FPS)
-print('w: {}, h: {}, fps: {}'.format(w, h, fps))
+P2Pro_resolution = (256, 384)
+P2Pro_fps = 25.0
 
-cap.set(cv2.CAP_PROP_FPS, 25)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 256)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 384)
-cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('Y','U','Y','V'))
+vid = cv2.VideoCapture(0)
+vid.set(cv2.CAP_PROP_CONVERT_RGB, 0)
 
-while True:
-    # Read frame from video capture
-    ret, frame = cap.read()
 
-    if not ret:
-        continue
+# Create a named window
+cv2.namedWindow("Thermal Frame", cv2.WINDOW_NORMAL)
+cv2.resizeWindow("Thermal Frame", 800, 600)
 
-    cv2.imshow('Frame', frame)
-    continue
 
-    # Crop bottom half of the frame
-    height, width, _ = frame.shape
-    cropped_frame = frame[int(height/2):height, :]
+while vid.isOpened():
+    empty, frame = vid.read()
 
-    # Convert pixel format to yuyv422
-    yuyv422_frame = cv2.cvtColor(cropped_frame, cv2.COLOR_BGR2YUV_Y422)
+    # split video frame (top is pseudo color, bottom is temperature data)
+    frame_mid_pos = int(len(frame) / 2)
+    picture_data = frame[0:frame_mid_pos]
+    thermal_data = frame[frame_mid_pos:]
 
-    # Display grayscale version of the frame
-    gray_frame = cv2.cvtColor(yuyv422_frame, cv2.COLOR_YUV2GRAY_10)
-    cv2.imshow('Grayscale', gray_frame)
+    # convert buffers to numpy arrays
+    yuv_picture = np.frombuffer(picture_data, dtype=np.uint8).reshape((P2Pro_resolution[1] // 2, P2Pro_resolution[0], 2))
+    rgb_picture = cv2.cvtColor(yuv_picture, cv2.COLOR_YUV2RGB_YUY2)
+    thermal_picture_16 = np.frombuffer(thermal_data, dtype=np.uint16).reshape((P2Pro_resolution[1] // 2, P2Pro_resolution[0]))
+    thermal_picture_8 = cv2.normalize(thermal_picture_16, None, 0, 255, cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+    colored_image = cv2.applyColorMap(thermal_picture_8, cv2.COLORMAP_INFERNO)
 
-    if cv2.waitKey(1) == ord('q'):
+
+
+    cv2.imshow("Thermal Frame", colored_image)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# Release video capture and close windows
-cap.release()
-cv2.destroyAllWindows()
+vid.release()
+print('done')
